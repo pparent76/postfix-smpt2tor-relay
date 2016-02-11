67db7e264336a2a59b88549d83c8e7bc531a35dc
Pierre Parent <pierre.parent@pparent.fr>
2016-02-11 14:39:43 +0100

    Add comment to make clear what the programm is for

--- src/genConfig.c
+++ src/genConfig.c
@@ -3,6 +3,12 @@
 
 main()
 {
+  /*************************************
+   * After this program will allow to choose
+   * custom config paramters like the db username
+   * and password, or the domain name handled (officially omb.one)
+   * ************************************/
+  
  printf("No custom parameter yet\n");
  return  0;
   
